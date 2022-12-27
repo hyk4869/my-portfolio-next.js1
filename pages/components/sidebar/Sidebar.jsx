bdd5868abf2c2eae42f9beeb25ef73214ece519c
hyk4869 <hyk4869@gmail.com>
2022-12-27 23:57:06 +0900

    ⚡️ フォルダ整理、サイドバー生成

---
+++ pages/components/sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const ITEM =[{ href: "/", img: "", title: "Home"},
+{ href: "/about", img: "", title: "About"},
+{ href: "/contact", img: "", title: "Contact"},
+{ href: "/skill", img: "", title: "Skill"},
+{ href: "/information", img: "", title: "Information"},]
+
+
+export function Sidebar() {
+  return (
+    <>
+      <div className={styles.menu}>
+        {ITEM.map(item =>{
+          return(
+
+            <ul key={item.href}  className={styles.ul}>
+              <li className={styles.li}>
+                <a href={item.href} className={styles.a} 
+                  target="_blank" rel="noopener noreferrer">
+                  {item.title}
+                </a>
+              </li>
+            </ul>
+
+          )
+        })}
+  
+      </div>
+    </>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { Sidebar } from './components/sidebar/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -117,6 +118,7 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <Sidebar/>
       </main>
     </>
   )
