34c227c6219cce02a36c5148357e36bc2f81e0e9
hyk4869 <hyk4869@gmail.com>
2022-12-28 00:10:10 +0900

    フォルダーの場所修正

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import { Sidebar } from './components/sidebar/Sidebar'
+import { Sidebar } from '../components/sidebar/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
