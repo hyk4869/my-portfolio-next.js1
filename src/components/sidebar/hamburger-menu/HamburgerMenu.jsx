5b17bf6c779a16ce48b2824873a1b7684a657889
hyk4869 <hyk4869@gmail.com>
2023-01-01 00:08:10 +0900

    修正

--- src/components/sidebar/hamburger-menu/HamburgerMenu.jsx
+++ src/components/sidebar/hamburger-menu/HamburgerMenu.jsx
@@ -1,4 +1,5 @@
 import { Ballet, Inter } from '@next/font/google'
+import { useState } from 'react';
 import styles from '../hamburger-menu/HamburgerMenu.module.css';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,14 +14,68 @@ const ITEM =[{ href: "/", icons: '/images/menu-icons/home.png', title: "Home"},
 
 
 export function HamburgerMenu() {
-  const [open, setOpen] = useState(false);
-  const toggleFunction = () => {
-    setOpen((prevState) => !prevState);
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    // setOpenMenu((prevState) => !prevState);
+    setOpenMenu(!openMenu);
   };
-  
+
   return (
     <>
+      <header id="header" className={styles.header}>
 
+        <div className={styles.menu}>
+          {ITEM.map(item =>{
+            return(
+              <ul key={item.href}  className={styles.ul}>
+                <li className={styles.li}>
+                    <a href={item.href} className={styles.a} target="_blank" rel="noopener noreferrer">
+                      <img src={item.icons} className={styles.icon}/>
+                      {item.title}
+                    </a>
+                </li>
+              </ul>
+            )
+          })}
+        </div>
+
+        <div className={styles.container}>
+          <div className={styles.humburger} onClick={() => menuFunction()}>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <p className={openMenu ? styles.open : undefined}>Menu</p>
+          </div>
+        </div>
+
+      </header>
+          
+
+
+
+      <div className={`${styles.drawerMenu} ${openMenu ? styles.open : undefined}`}>
+        <ul>
+          <div className={styles.close} onClick={() => menuFunction()}>
+            <span></span>
+            <span></span>
+            <p>Close</p>
+          </div>
+          {/* <div className={styles.menu}>
+            {ITEM.map(item =>{
+              return(
+                <ul key={item.href}  className={styles.ul}>
+                  <li className={styles.li}>
+                      <a href={item.href} className={styles.a} target="_blank" rel="noopener noreferrer">
+                        <img src={item.icons} className={styles.icon}/>
+                        {item.title}
+                      </a>
+                  </li>
+                </ul>
+              )
+            })}
+
+          </div> */}
+        </ul>
+      </div>
     </>
   )
 }
