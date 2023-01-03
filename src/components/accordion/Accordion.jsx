0b15e98253b4d5fb5f861d3384dba6027fe6d8f4
hyk4869 <hyk4869@gmail.com>
2023-01-03 18:23:03 +0900

    faq作成

---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../accordion/Accordion.module.css'
+import { AccordionData } from './AccordionData'
+
+export function Accordion() {
+  return (
+    <>
+        <AccordionData/>
+    
+    </>
+  )
+}
---
+++ src/components/accordion/AccordionData.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styles from '../accordion/Accordion.module.css'
+
+
+export function AccordionData() {
+
+    const Data =[
+        {
+            question:'どのような技術を使いましたか？',
+            answer:'JavaScriptのReatを使用しました。'
+        },
+        {
+            question:'学習したプログラミング言語は何ですか？',
+            answer:'HTML、CSS、JavaScript、Pythonです。'
+        },
+        {
+            question:'今後学習したい技術は何ですか？',
+            answer:'PHP、Larabel、AWS、ネットワーク全般、サーバー全般'
+        },
+        {
+            question:'コーヒーは好きですか？',
+            answer:'大好きです。'
+        },
+        {
+            question:'質問・テスト長文、質問・テスト長文、質問・テスト長文、質問・テスト長文、質問・テスト長文、質問・テスト長文、質問・テスト長文、質問・テスト長文、質問・テスト長文',
+            answer:'回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、回答・テスト長文、'
+        }
+    ]
+
+  return (
+    <>
+        <div className={styles.faqParents}>
+            {Data.map(item =>{
+                return(
+                    <div key={item.question} className={styles.child}>
+
+                        <div className={styles.grandchild}>
+
+                            <div className={styles.questionParents}>
+                                <h2 className={styles.question}>
+                                    {item.question} 
+                                </h2>
+                                <span className={styles.span}>+</span>
+
+
+                            </div>
+
+                            <div className={styles.answerParents}>
+                                <p className={styles.answer}>
+                                    {item.answer}
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                )
+            })}
+        </div>
+    </>
+  )
+}
