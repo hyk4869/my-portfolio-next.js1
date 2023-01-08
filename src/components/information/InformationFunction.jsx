237631524b29c0461c10ceb3bbec7f52849f405e
hyk4869 <hyk4869@gmail.com>
2023-01-08 15:47:20 +0900

    修正

--- src/components/information/InformationFunction.jsx
+++ src/components/information/InformationFunction.jsx
@@ -2,21 +2,27 @@ import Image from 'next/image'
 import React from 'react'
 import { InformationData } from './InformationData'
 import styles from '../information/Information.module.css'
+import Link from 'next/link'
 
 export function InformationFunction({Data}) {
+  const handleClick=(() => {
+    alert('clicked')
+  
+  });
+  
   return (
     <div className={styles.parents}>
       {InformationData.map((Data) =>{
         return(
           <div className={styles.master} key={Data.image}>
-              <div className={styles.children}>
-                <Image src={Data.image} width={400} height={200} alt='InformationImage' className={styles.image} onClick={handleClick}></Image>
-              </div>
+            <div className={styles.children}>
+              <Image src={Data.image} width={400} height={200} alt='InformationImage' className={styles.image} onClick={handleClick}></Image>
+            </div>
 
-              <div className={styles.contents}>
-                <h2 className={styles.h2}>{Data.title}</h2>
-                <p className={styles.p}>{Data.description}</p>
-              </div>
+            <div className={styles.contents}>
+              <h2 className={styles.h2}>{Data.title}</h2>
+              <p className={styles.p}>{Data.description}</p>
+            </div>
           </div>
         )
 
@@ -26,8 +32,5 @@ export function InformationFunction({Data}) {
   )
 }
 
-const handleClick=(() => {
-  alert('clicked')
 
-});
 
