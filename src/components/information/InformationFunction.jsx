f1f91d9cf99a56a05b67e3d2d1ac638b66a229cc
hyk4869 <hyk4869@gmail.com>
2023-01-08 11:54:40 +0900

    upload image

---
+++ src/components/information/InformationFunction.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function InformationFunction() {
+  return (
+    <div>Information</div>
+  )
+}
---
+++ src/pages/information.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+import { InformationFunction } from '../components/information/InformationFunction'
+
+export default function Information() {
+  return (
+    <>
+    <InformationFunction/>
+      
+    </>
+  )
+}
+
