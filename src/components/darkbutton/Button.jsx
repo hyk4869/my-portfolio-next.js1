8587678cfd860acc1fbc49c8be370321f9bd9518
hyk4869 <hyk4869@gmail.com>
2023-01-09 20:30:04 +0900

    修正

--- src/components/darkbutton/Button.jsx
+++ src/components/darkbutton/Button.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../darkbutton/Button.module.css'
 
 export function Button() {
+  const[dark,setDark] = useState(false);
+  
+  /*const handleClick = (e) =>{
+    if (e.target.checked === true){
+      return console.log("クリックされました")
+    }else{
+      return console.log("外れました")
+    }
+  }
+
+  const handleClick = (e) =>{
+    (e.target.checked === true)? console.log("クリック"):console.log("外れました")
+  } */
+
+  const handleClick = (e) =>{
+    setDark((dark) => !dark);
+    `${(e.target.checked === true)? console.log("クリック") : console.log("外れました")}`
+  }
+
   return (
     <>
-      <div className={styles.checkbtnParents}>
-        <input type='checkbox' id='checkbox' name='checkbox'/>
-        <label htmlFor='checkbox'>チェック</label>
-      </div>
+      <div className={`${styles.checkbtnParents} ${dark ? styles.checkbtnParents2 : styles.checkbtnParents}`}
+        //className={styles.checkbtnParents} 
+        onChange={handleClick}>
+        <input type='checkbox' id='checkbox' name='checkbox' className={styles.input}/>
+        <label htmlFor='checkbox' className={styles.label}>チェック</label>
+      </div> 
     </>
   )
 }
+
+{/* <div className={`${css.main_css} ${dark ? css.main_css2 : css.main_css}`} onChange={handleClick}></div> */}
+
 
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,10 @@
 import { HamburgerMenu } from '../components/sidebar/hamburger-menu/HamburgerMenu';
 import '../styles/globals.css'
+import styles from '../styles/Home.module.css'
 
 
 export default function App({ Component, pageProps }) {
+
   return (
     <div>
       <HamburgerMenu/>
